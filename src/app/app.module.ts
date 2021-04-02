d0da15d946c7b0448d191e698a4b0893bea9faa5
Felipe Correia <felipedreamcorreia@hotmail.com>
2021-04-02 02:32:25 -0300

    feature: first page in angular 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PrimeiroComponente } from './componentes/primeiro.component';
+import { PageHomeComponent } from './page-home/page-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponente,
+    PageHomeComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/componentes/primeiro.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'componente-um',
+  template: `<p>Teste de primeiro componente</p>`
+})
+export class PrimeiroComponente {}
